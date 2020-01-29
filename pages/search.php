d9f601044caa90df51669b3f21614ab82c67d815
Yaserson95 <55657563+Yaserson95@users.noreply.github.com>
2020-01-29 21:51:00 +0300

    Update search.php

--- pages/search.php
+++ pages/search.php
@@ -2,7 +2,7 @@
 //include_once	'function.php';
 include_once	'fileSearcer.php';
 if(!isset($_POST["Key"])||!isset($_POST["Filename"])){
-				exit("Нет данных");
+	exit("Нет данных");
 }
 $filename = $_SERVER["DOCUMENT_ROOT"]."/files/".$_POST["Filename"];
 $searcer = new FileSearcher($filename);
@@ -13,26 +13,24 @@ $rezult = $searcer->binarySearch($_POST["Key"],0,$searcer->getLength());
 ?>
 <!DOCTYPE html>
 <html>
-				<head>
-								<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-								<title>Тестик</title>
-				</head>
-				<body>
-								<h1>Результаты поиска</h1>
-								<p>По запросу: "<?php echo $_POST["Key"];?>" 
-								<?php
-												if($rezult=="undef"){
-																echo "ничего не найдено!";
-												}
-												else{
-																echo "найдено:";
-												}
-												echo "<p><b>".$rezult->getKey()."</b> ".$rezult->getValue()."</p>";
-								?>
-								<a href="/">Назад</a>
-								</p>
-				</body>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Тестик</title>
+	</head>
+	<body>
+		<h1>Результаты поиска</h1>
+		<p>По запросу: "<?php echo $_POST["Key"];?>" 
+		<?php
+			if($rezult=="undef"){
+				echo "ничего не найдено!";
+			}
+			else{
+				echo "найдено:";
+				echo "<p><b>".$rezult->getKey()."</b> ".$rezult->getValue()."</p>";
+			}
+			
+		?>
+		<a href="/">Назад</a>
+		</p>
+	</body>
 </html>
-
-
-
